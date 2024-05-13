cd825cf9b8325fbe9c7d8c4748e98bc6ebe6c175
YI-JYUN HE <114564290+heyi5478@users.noreply.github.com>
2024-05-13 14:35:37 +0800

    [ fix ] 修改圖片

--- js/all.js
+++ js/all.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
 /*0000視差滾動0000000*/
-  $('.parallax-window01').parallax({imageSrc:'images/bg-1.jpg'});
-  $('.parallax-window02').parallax({imageSrc:'images/bg02.jpg'});
-  $('.parallax-window03').parallax({imageSrc:'images/bg-1.jpg'});
+  $('.parallax-window01').parallax({imageSrc:'images/bg-2.jpg'});
+  $('.parallax-window02').parallax({imageSrc:'images/bg03.jpg'});
+  $('.parallax-window03').parallax({imageSrc:'images/bg-2.jpg'});
 
 /*漢堡選單*/
  $('.showmenu').on('click',  function(e){
